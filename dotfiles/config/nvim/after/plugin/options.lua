e917f94f90187d864dbec542d3f674053f0effb3
Adilson Santos <adilson.santos97@live.com>
2024-01-03 01:25:02 -0300

    2.9.3
    Updated dotdrop submodule.
    
    Updated nvim config to use tabs by default and added keybinds to allow
    move content up and down.
    
    Replaced exa with eza.
    
    Removed thefuck and cut content to fit the width of the ruler

--- dotfiles/config/nvim/after/plugin/options.lua
+++ dotfiles/config/nvim/after/plugin/options.lua
@@ -3,11 +3,11 @@ local opt = vim.opt
 opt.relativenumber = true
 
 opt.colorcolumn = "100"
-opt.expandtab = true
-opt.shiftwidth = 2
+opt.shiftwidth = 4
+opt.softtabstop = 4
 opt.smartindent = true
 opt.textwidth = 100
-opt.tabstop = 2
+opt.tabstop = 8
 
 opt.incsearch = true
 opt.inccommand = "split"
